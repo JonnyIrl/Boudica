31f60bc32a3fe71f924fd79cd26952592f989088
Jonathan Coffey <swtorjonny@hotmail.com>
2022-12-22 09:39:06 +0000

    Added daily gift command which users can interact with to get random amounts of glimmer each day

---
+++ Boudica/Services/DailyGiftService.cs
@@ -0,0 +1,38 @@
+﻿using Boudica.MongoDB;
+using Boudica.MongoDB.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boudica.Services
+{
+    public class DailyGiftService
+    {
+        private readonly IMongoDBContext _mongoDBContext;
+        protected IMongoCollection<DailyGift> _dailyGiftCollection;
+        public DailyGiftService(IMongoDBContext mongoDBContext)
+        {
+            _mongoDBContext = mongoDBContext;
+            string name = typeof(DailyGift).Name;
+            _dailyGiftCollection = _mongoDBContext.GetCollection<DailyGift>(name);
+        }
+
+        public async Task<DailyGift> Get(ulong userId)
+        {
+            return await _dailyGiftCollection.Find(x => x.Id == userId).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> UpsertUsersDailyGift(ulong userId)
+        {
+            var builder = Builders<DailyGift>.Filter;
+            var updateBuilder = Builders<DailyGift>.Update;
+            var filter = builder.Eq(x => x.Id, userId);
+            var update = updateBuilder.Set(x => x.DateTimeLastGifted, DateTime.UtcNow);
+            var result = await _dailyGiftCollection.FindOneAndUpdateAsync(filter, update, new FindOneAndUpdateOptions<DailyGift, DailyGift>() { IsUpsert = true, ReturnDocument = ReturnDocument.After });
+            return result != null;
+        }
+    }
+}
