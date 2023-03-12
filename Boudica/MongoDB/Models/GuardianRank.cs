6d892476aca12c098f9ee1a0f94d7c73f1da7931
Jonathan Coffey <swtorjonny@hotmail.com>
2023-03-12 12:32:51 +0000

    Started work on Guardian Rank

---
+++ Boudica/MongoDB/Models/GuardianRank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boudica.MongoDB.Models
+{
+    public class GuardianRank
+    {
+        public ulong Id { get; set; }
+        public string Username { get; set; }
+        public int XP { get; set; }
+        public DateTime DateTimeLastInteractedWithDiscord { get; set; }
+    }
+}
---
+++ Boudica/Services/RankingService.cs
@@ -0,0 +1,27 @@
+﻿using Boudica.MongoDB.Models;
+using Boudica.MongoDB;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boudica.Services
+{
+    public class RankingService
+    {
+        private readonly IMongoDBContext _mongoDBContext;
+        protected IMongoCollection<Insult> _insultCollection;
+        public RankingService(IMongoDBContext mongoDBContext)
+        {
+            _mongoDBContext = mongoDBContext;
+#if DEBUG
+            string name = typeof(Insult).Name + "Test";
+#else
+            string name = typeof(Insult).Name;
+#endif
+            _insultCollection = _mongoDBContext.GetCollection<Insult>(name);
+        }
+    }
+}
