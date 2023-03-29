e288dfc5b21dc57d504a5fab3938c6f5e80172f4
Jonathan Coffey <swtorjonny@hotmail.com>
2023-03-29 06:59:52 +0000

    Barebones rank enum done

---
+++ Boudica/Enums/Rank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boudica.Enums
+{
+    public enum Rank
+    {
+        Unranked,
+        Bronze1,
+        Bronze2,
+        Bronze3,
+        Silver1,
+        Silver2,
+        Silver3,
+        Gold1,
+        Gold2,
+        Gold3,
+        Platinum1,
+        Platinum2,
+        Platinum3,
+        Diamond1,
+        Diamond2,
+        Diamond3,
+        Master,
+        GrandMaster
+    }
+}
