fbb8c8673f901052cb89021f748bb391e824e5df
Jonathan Coffey <swtorjonny@hotmail.com>
2023-04-03 06:50:29 +0000

    Added some updates to Ranking system, still needs work

--- Boudica/Classes/RankingSystem.cs
+++ Boudica/Classes/RankingSystem.cs
@@ -1,82 +1,36 @@
-﻿using System;
+﻿using Boudica.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Boudica.Classes
-{
-    //    Example Useage
-    //    RankingSystem rankingSystem = new RankingSystem();
-    //    rankingSystem.Score(150); // Score is 150 (Bronze III)
-    //    rankingSystem.Score(200); // Score is 350 (Silver III)
-    //    rankingSystem.Score(450); // Score is 800 (Gold I)
-    //    string currentRank = rankingSystem.GetRank();
-    //    Console.WriteLine($"Current rank: {currentRank}"); // Outputs "Gold I"
-
-    public enum RankType
-    {
-        BronzeIII = 1,
-        BronzeII = 2,
-        BronzeI = 3,
-        SilverIII = 4,
-        SilverII = 5,
-        SilverI = 6,
-        GoldIII = 7,
-        GoldII = 8,
-        GoldI = 9,
-        DiamondIII = 10,
-        DiamondII = 11,
-        DiamondI = 12,
-        PlatinumIII = 13,
-        PlatinumII = 14,
-        PlatinumI = 15
-    }
-
-    public class Rank
-    {
-        public RankType RankType { get; set; }
-        public int MajorRank { get; set; }
-        public int MinorRank { get; set; }
-        public int MinimumScore { get; set; }
-
-        public Rank(RankType rankType, int majorRank, int minorRank, int minimumScore)
-        {
-            RankType = rankType;
-            MajorRank = majorRank;
-            MinorRank = minorRank;
-            MinimumScore = minimumScore;
-        }
-    }
-
+{ 
     public class RankingSystem
     {
-        private Dictionary<RankType, Rank> _ranks;
+        private static Dictionary<RankType, int> _ranks;
 
         public RankingSystem()
         {
-            _ranks = new Dictionary<RankType, Rank>();
-
-            // Initialize the ranks
-            int score = 0;
-            for (int majorRank = 1; majorRank <= 5; majorRank++)
+            if (_ranks == null)
             {
-                for (int minorRank = 3; minorRank >= 1; minorRank--)
+                _ranks = new Dictionary<RankType, int>();
+                for (int majorRank = 1; majorRank <= 5; majorRank++)
                 {
-                    score += CalculateMinorRankStep(majorRank, minorRank);
-
-                    var rankType = (RankType)((majorRank - 1) * 3 + minorRank);
-                    var rank = new Rank(rankType, majorRank, minorRank, score);
-
-                    _ranks[rankType] = rank;
+                    for (int minorRank = 3; minorRank >= 1; minorRank--)
+                    {
+                        int scoreCapForRank = CalculateMinorRankStep(majorRank, minorRank);
+                        RankType rankType = (RankType)((majorRank - 1) * 3 + minorRank);
+                        _ranks[rankType] = scoreCapForRank;
+                    }
                 }
             }
         }
 
-        public Rank GetRank(int score)
+        public GuardianRank GetRank(int score)
         {
-            var rankType = _ranks.Keys.LastOrDefault(k => _ranks[k].MinimumScore <= score);
-            return _ranks[rankType];
+            return new GuardianRank(RankType.BronzeIII, 0);
         }
 
         private int CalculateMinorRankStep(int majorRank, int minorRank)
---
+++ Boudica/Enums/RankType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boudica.Enums
+{
+    public enum RankType
+    {
+        BronzeIII = 1,
+        BronzeII = 2,
+        BronzeI = 3,
+        SilverIII = 4,
+        SilverII = 5,
+        SilverI = 6,
+        GoldIII = 7,
+        GoldII = 8,
+        GoldI = 9,
+        DiamondIII = 10,
+        DiamondII = 11,
+        DiamondI = 12,
+        PlatinumIII = 13,
+        PlatinumII = 14,
+        PlatinumI = 15
+    }
+}
